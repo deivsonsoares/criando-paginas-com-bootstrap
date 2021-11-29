53e1448e4317173c85a2a77fc0b3d941f15d7879
deivsonsoares <deivsonds.ds@gmail.com>
2021-11-28 23:28:38 -0300

    Finalizacao do projeto aula bootstrap

---
+++ introducao-bootstrap/script.js
@@ -0,0 +1,20 @@
+function bootstrap(){
+    //outra aba
+    window.open("https://getbootstrap.com/docs/5.1/getting-started/download/");  
+    //mesma janela (opcap de voltar)
+    //window.location.href = "https://web.dio.me/";
+}
+
+function color_codes(){
+    //outra aba
+    window.open("https://html-color-codes.info/");  
+    //mesma janela (opcap de voltar)
+    //window.location.href = "https://web.dio.me/";
+}
+
+function pexels(){
+    //outra aba
+    window.open("https://www.pexels.com/");  
+    //mesma janela (opcap de voltar)
+    //window.location.href = "https://web.dio.me/";
+}
